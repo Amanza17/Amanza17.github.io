e73781b3fab6f4b0f8ed0c2941f3637219be5f15
Amanza17 <a.manzanares.2023@alumnos.urjc.es>
2025-01-22 15:52:11 +0100

    first commit

---
+++ aboutMe.js
@@ -0,0 +1,2 @@
+
+// JavaScript code can be added here in the future
---
+++ scripts.js
@@ -0,0 +1,4 @@
+function showDevelopmentMessage(event) {
+    event.preventDefault();
+    alert("This section is under development and cannot be viewed at the moment.");
+}
