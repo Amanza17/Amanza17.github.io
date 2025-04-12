1c7404790aa81a19cbbfaa795513db2fbfa61811
Amanza17 <157617646+Amanza17@users.noreply.github.com>
2025-04-12 13:30:56 +0200

    Update nfc.js

--- nfc.js
+++ nfc.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-    window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+    window.location.href = "https://www.youtube.com/watch?v=CtBt436SGdU";
 };
